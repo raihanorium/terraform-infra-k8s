4790f5db3a42f0b0c48ab35cb87c8343f5f51faf
Raihan <ataul.raihan@ideascale.com>
2025-01-27 12:30:59 +0600

    Kind cluster

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "kind_cluster" "kind-cluster" {
+  name = "k8s-cluster"
+  kubeconfig_path = pathexpand("~/.kube/config")
+  node_image = "kindest/node:v1.27.1"
+  wait_for_ready = true
+
+  kind_config {
+    kind = "Cluster"
+    api_version = "kind.x-k8s.io/v1alpha4"
+
+    node {
+      role = "control-plane"
+    }
+    node {
+      role =  "worker"
+    }
+    node {
+      role =  "worker"
+    }
+  }
+}
