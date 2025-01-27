5be0fd9852ba5c2376037306927f567ff2f83fc6
Raihan <ataul.raihan@ideascale.com>
2025-01-27 19:58:54 +0600

    Dashboard

--- main.tf
+++ main.tf
@@ -10,6 +10,17 @@ resource "kind_cluster" "kind-cluster" {
 
     node {
       role = "control-plane"
+      kubeadm_config_patches = [
+        "kind: InitConfiguration\nnodeRegistration:\n  kubeletExtraArgs:\n    node-labels: \"ingress-ready=true\"\n"
+      ]
+      extra_port_mappings {
+        container_port = 80
+        host_port = 80
+      }
+      extra_port_mappings {
+        container_port = 443
+        host_port = 443
+      }
     }
     node {
       role =  "worker"
