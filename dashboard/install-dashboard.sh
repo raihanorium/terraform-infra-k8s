5e137e41725ff7c9faae94460f63e3a406004cd6
Ataul Raihan <ataul.raihan@ideascale.com>
2025-02-23 19:18:58 +0600

    Add Kubernetes dashboard installation and configuration files

---
+++ dashboard/install-dashboard.sh
@@ -0,0 +1,18 @@
+# install dashboard
+helm upgrade --install kubernetes-dashboard kubernetes-dashboard \
+    --repo https://kubernetes.github.io/dashboard/ \
+    --version 7.10.4 \
+    --namespace kubernetes-dashboard --create-namespace \
+    --set "extraArgs={'--enable-skip-login','--enable-insecure-login'},protocolHttp=true,metricsScraper.enabled=true"
+  kubectl wait pods -n kubernetes-dashboard -l app.kubernetes.io/name=kubernetes-dashboard --for condition=Ready --timeout=180s >/dev/null 2>&1
+  kubectl apply -f "dashboard_rbac.yaml"
+
+  # setup kubernetes dashboard ingress
+#  kubectl -n kubernetes-dashboard create secret tls dashboard-tls --key="${DIR}/../config/certs/ingress/ideascale.me/cert.key" --cert="${DIR}/../config/certs/ingress/ideascale.me/cert.pem"
+
+  # create dashboard ingress
+  kubectl apply -f "dashboard_ingress.yaml"
+
+  if [[ "${DEPLOY_ISTIO}" == 'true' ]]; then
+    configure_namespace_for_istio kubernetes-dashboard
+  fi
