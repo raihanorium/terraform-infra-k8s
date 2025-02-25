136bcc17e2c7ca5da8f48199713907c876c8b1c1
Ataul Raihan <ataul.raihan@ideascale.com>
2025-02-25 19:33:33 +0600

    Use environment variable for user in k3sup installation script

--- install-k3s.sh
+++ install-k3s.sh
@@ -1,5 +1,5 @@
 k3sup install --local \
-    --user roo \
+    --user "${USER}" \
     --k3s-version v1.31.5+k3s1 \
     --k3s-extra-args '--disable=traefik --docker --write-kubeconfig-mode=644' \
     --local-path "${HOME}/.kube/config"
